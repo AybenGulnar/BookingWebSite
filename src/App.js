14c72763e92a9d0b5d86d5cec3aeb2f9fbe9d5a1
DESKTOP-4C73P7U\ayben <ayben.glnar@gmail.com>
2022-05-14 01:39:57 +0300

    booking

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from "react";
+import {
+  BrowserRouter,
+  Route,
+  Routes
+} from "react-router-dom";
+import Home from "./pages/home/Home";
+import List from "./pages/list/List";
+import Hotel from "./pages/hotel/Hotel";
+
+
+
+
+function App() {
+  return (
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/hotels" element={<List />} />
+      <Route path="/hotels/:id" element={<Hotel />} />
+      
+    </Routes>
+    </BrowserRouter>
+
+  );
+}
+
+export default App;
